47fb7dda32f85d36473f246202567fa6754e08fb
bilguunsodnom417 <bilguunsodnom417@gmail.com>
2022-03-18 17:00:19 +0800

    update

---
+++ src/common/more-button/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MoreButtonStyle = styled.div`
+span{
+padding: 10px;
+}
+a{    
+color:#345BB8;
+text-decoration:none;
+padding-top:4px;
+
+}
+`;

d23f58dd54fccc86e057acf3cf96bbf00335f5df
Bilegsaihan <bilegprogramming@gmail.com>
2022-04-19 20:54:21 +0800

    update

--- src/components/common/slider/index.tsx
+++ src/components/common/slider/index.tsx
@@ -1,49 +1,38 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import SimpleImageSlider from "react-simple-image-slider";
 
-const Slider = (props: any) => {
-  const { postMode } = props;
-  let [imageSrc, setImages] = useState([]);
+const Slider = ({postMode}: any) => {
+    const [images, setImages]: any = useState([]);
+    const imgBase = process.env.NEXT_PUBLIC_FILE_HOST;
 
-  const imgBase = process.env.NEXT_PUBLIC_FILE_HOST;
+    useEffect(() => {
+        let imgs = [];
+        let url;
+        for (let img of postMode.images) {
+            url = "" + imgBase + img.filePath;
+            console.log("====== URL: ", url);
+            imgs.push({
+                url: "" + imgBase + img.filePath
+            });
+        }
+        setImages(imgs);
+    }, []);
 
-  // const imgSrc =
-  //   postMode.images[0] && postMode.images[0].filePath
-  //     ? imgBase + postMode.images[0].filePath
-  //     : null;
-
-  useEffect(() => {
-    let imgSrc = postMode.images.map((e: any) => e);
-
-    setImages(
-      imgSrc[0] && imgSrc[0].filePath ? imgBase + imgSrc[0].filePath : null
+    return (
+        <div>
+            <>
+                {/* {imageData.map((img: any, index: number) => { */}
+                {/* console.log(img, "img"); */}
+                <SimpleImageSlider
+                    width={"80%"}
+                    height={"500px"}
+                    images={images}
+                    showBullets={false}
+                    showNavs={true}
+                />
+                {/* })} */}
+            </>
+        </div>
     );
-  }, []);
-
-  const imageData = [
-    {
-      url: imageSrc,
-    },
-  ];
-  const images1 = [{ url: "/image/education.png" }, { url: "/icon/exit.svg" }];
-  console.log(images1, "imgimgimgimgimg");
-  console.log(imageSrc, "imageDataimageDataimageDataimageData");
-
-  return (
-    <div>
-      <>
-        {/* {imageData.map((img: any, index: number) => { */}
-        {/* console.log(img, "img"); */}
-        <SimpleImageSlider
-          width={"80%"}
-          height={"500px"}
-          images={imageData}
-          showBullets={false}
-          showNavs={true}
-        />
-        {/* })} */}
-      </>
-    </div>
-  );
 };
 export default Slider;

ab2e5a621e1f22669353f2e3b03d4821e516d1cb
bilguunsodnom417 <bilguunsodnom417@gmail.com>
2022-04-21 13:11:18 +0800

    update

---
+++ src/components/widget/index.tsx
@@ -0,0 +1,69 @@
+import AvatarCard from "components/common/avatar-card";
+import MoreButton from "components/common/more-button";
+import News from "components/common/news";
+import { AppNewsBig } from "components/common/news/news";
+import { AppNewsSmall } from "components/common/news/news-small";
+import { NewsStyle } from "components/common/news/style";
+import { WidgetAPI } from "lib/api/widget";
+import React, { useEffect, useState } from "react";
+
+const Widget = ({}: any) => {
+  const [news, setNews]: any = useState([]);
+
+  const loadEmployees = async () => {
+    const emps = await WidgetAPI.all();
+    setNews(emps);
+
+    console.log("pororoorororo", emps);
+  };
+
+  useEffect(() => {
+    loadEmployees();
+  }, []);
+
+  return (
+    <>
+      <NewsStyle>
+        <section id="news_list" className="news_list section">
+          <div className="header-container container">
+            <div className="row">
+              <div className="col-lg">
+                <h1>UFE мэдээ</h1>
+              </div>
+              <div className="col-lg">
+                <div className="more-button">
+                  <MoreButton />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row gx-5">
+              {news.map((news: any, index: number) => {
+                return (
+                  <>
+                    {index == 0 && (
+                      <div className="col-lg-8">
+                        <div key={news.id}>
+                          <AppNewsBig newsData={news} />
+                        </div>
+                      </div>
+                    )}
+                    {index > 0 && (
+                      <div className="col-lg-4">
+                        <div key={news.id}>
+                          <AppNewsSmall newsData={news} />
+                        </div>
+                      </div>
+                    )}
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      </NewsStyle>
+    </>
+  );
+};
+export default Widget;
---
+++ src/lib/api/widget.tsx
@@ -0,0 +1,55 @@
+import useSWR from "swr";
+import axios from "config/axios";
+
+const urlPrefix = "/post/widget/list";
+export const listUrl = `${urlPrefix}`;
+
+export const WidgetSWR = () => {
+  const fetcher = async (url: any) =>
+    await axios.get(`${urlPrefix}`).then((res: any) => {
+      console.log(res);
+      return res.data.posts;
+    });
+  return useSWR(listUrl, fetcher);
+};
+console.log(listUrl);
+export const WidgetAPI = {
+  all: async () => {
+    const { data, status } = await axios.get(listUrl);
+    if (status != 200) {
+      return [];
+    }
+    console.log(data, "flfkffkfkfkkfkf");
+
+    return data.posts;
+  },
+
+  new: async (values: any) => {
+    const { data, status } = await axios.post(`${urlPrefix}/New`, values);
+
+    return {
+      data,
+      status,
+    };
+  },
+
+  update: async (id: any, values: any) => {
+    const { data, status } = await axios.put(`${urlPrefix}/${id}`, values);
+
+    return {
+      data,
+      status,
+    };
+  },
+
+  delete: async (id: any) => {
+    const { data, status } = await axios.post(`${urlPrefix}/Delete`, {
+      RoomTypeId: id,
+    });
+
+    return {
+      data,
+      status,
+    };
+  },
+};

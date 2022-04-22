94e16d83e07fc0a201cccc0e2afea176676abfe7
bilguunsodnom417 <bilguunsodnom417@gmail.com>
2022-04-22 13:19:20 +0800

    update

---
+++ src/pages/widgetDetail/index.tsx
@@ -0,0 +1,18 @@
+import { CardStyle } from "components/common/card/style";
+
+const WidgetDetail = () => {
+  return (
+    <CardStyle>
+      <section id="card_list" className="card_list section-bg">
+        <div className="container">
+          <div className="col-md-12">
+            <div className={"row"}>
+              <button>ffddf</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </CardStyle>
+  );
+};
+export default WidgetDetail;

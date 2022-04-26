b1e87eb8caadb9925bc59cf90f0986ce6c7d1cfd
bilguunsodnom417 <bilguunsodnom417@gmail.com>
2022-04-26 23:25:21 +0800

    update

--- src/components/common/card/index.tsx
+++ src/components/common/card/index.tsx
@@ -1,25 +1,13 @@
 import React from "react";
-import { CardStyle } from "./style";
-import CardImage from "../../../assets/images/card_image.png";
-import Image from "next/image";
+
 import CustomButton from "../custom-button";
-import MoreButton from "../more-button";
-import nextConfig from "../../../../next.config";
 
 const CustomCard = ({ name, shortName, imagePath }: any) => (
   <div className="col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
     <div className="card">
       <div className="hover-zoomin">
         <a>
-          {/* {imagePath}
-          -ddsdsdsd- */}
-          {/* <Image
-            src={`https://api.ufe.edu.mn/${imagePath}`}
-            alt="image-alt-text"
-            width={500}
-            height={500}
-          /> */}
-          {/* <Image src={`${imagePath}`} width={500} height={500} /> */}
+
         </a>
       </div>
 
